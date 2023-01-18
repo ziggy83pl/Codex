149da4cf731edee6479c9e1f4b1fa5758d351029
ziggy83pl <zbyszekszczesny83@gmail.com>
2023-01-18 20:46:48 +0100

    update the api key

--- client/script.js
+++ client/script.js
@@ -85,8 +85,8 @@ const handleSubmit = async (e) => {
 
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
-
-    const response = await fetch('https://codex-95w6.onrender.com', {
+//https://codex-95w6.onrender.com/
+    const response = await fetch('http://localhost:5000', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
--- server/server.js
+++ server/server.js
@@ -45,4 +45,4 @@ app.post('/', async (req, res) => {
   }
 })
 
-app.listen(5000, () => console.log('AI server started on https://codex-95w6.onrender.com'));
+app.listen(5000, () => console.log('AI server started on http://localhost:5000'));
